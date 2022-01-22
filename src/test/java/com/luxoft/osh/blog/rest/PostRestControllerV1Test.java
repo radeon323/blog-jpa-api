d6be1257d05086b4501863cb19067c1ca0423153
Oleksandr Shevchenko <4336399@gmail.com>
2022-01-22 21:48:39 +0200

    Tests added

--- src/test/java/com/luxoft/osh/blog/rest/PostRestControllerV1Test.java
+++ src/test/java/com/luxoft/osh/blog/rest/PostRestControllerV1Test.java
@@ -2,8 +2,10 @@ package com.luxoft.osh.blog.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.luxoft.osh.blog.entity.Post;
+import com.luxoft.osh.blog.repository.PostRepository;
 import com.luxoft.osh.blog.service.PostService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,6 +15,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -30,12 +33,16 @@ class PostRestControllerV1Test {
     @MockBean
     private PostService postService;
 
+    @MockBean
+    private PostRepository postRepository;
+
+
     private final String API_URL = "/api/v1/posts/";
 
     @Test
     void testGetAllPosts() throws Exception {
         List<Post> posts = prepareForTestsAndReturnListOfPosts();
-        given(postService.getAll()).willReturn(posts);
+        given(postService.findAll()).willReturn(posts);
         mockMvc.perform( MockMvcRequestBuilders
                 .get(API_URL)
                 .accept(MediaType.APPLICATION_JSON))
@@ -46,6 +53,39 @@ class PostRestControllerV1Test {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].title").value("Second post"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").doesNotExist())
                 .andExpect(MockMvcResultMatchers.jsonPath("$[2].star").isBoolean());
+    }
+
+    @Test
+    void testSortByTitle() throws Exception {
+        List<Post> posts = prepareForTestsAndReturnListOfPosts();
+        posts.sort(new Comparator<Post>() {
+            @Override
+            public int compare(Post p1, Post p2) {
+                return p1.getTitle().compareTo(p2.getTitle());
+            }
+        });
+        given(postService.sortByTitle()).willReturn(posts);
+        mockMvc.perform( MockMvcRequestBuilders
+                .get(API_URL + "?sort=title")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].title").value("A Third post"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].title").value("First post"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].title").value("Second post"));
+    }
+
+    @Test
+    void testFindByTitle() throws Exception {
+        List<Post> posts = List.of(prepareForTestsAndReturnListOfPosts().get(0));
+        given(postService.findByTitle("First post")).willReturn(posts);
+        mockMvc.perform( MockMvcRequestBuilders
+                .get(API_URL + "?title={title}", "First post")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].title").value("First post"));
     }
 
     @Test
@@ -64,7 +104,9 @@ class PostRestControllerV1Test {
     @Test
     void testSavePost() throws Exception {
         Post post = prepareForTestsAndReturnListOfPosts().get(1);
+        Mockito.when(postRepository.save(post)).thenReturn(post);
         String json = asJsonString(post);
+
         mockMvc.perform( MockMvcRequestBuilders
                 .post(API_URL, 2)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -86,7 +128,7 @@ class PostRestControllerV1Test {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(3))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Third post"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("A Third post"));
     }
 
     @Test
@@ -138,31 +180,32 @@ class PostRestControllerV1Test {
 
 
 
+
     private List<Post> prepareForTestsAndReturnListOfPosts() {
         List<Post> posts = new ArrayList<>();
 
         Post firstPost = Post.builder()
+                .id(1L)
                 .title("First post")
                 .content("A baba galamaga")
                 .star(true)
                 .build();
-        firstPost.setId(1L);
         posts.add(firstPost);
 
         Post secondPost = Post.builder()
+                .id(2L)
                 .title("Second post")
                 .content("Eins, zwei, Polizei")
                 .star(false)
                 .build();
-        secondPost.setId(2L);
         posts.add(secondPost);
 
         Post thirdPost = Post.builder()
-                .title("Third post")
+                .id(3L)
+                .title("A Third post")
                 .content("Padav snih na porih, Kit zlipyv sobi pyrih.")
                 .star(true)
                 .build();
-        thirdPost.setId(3L);
         posts.add(thirdPost);
 
         return posts;
@@ -175,6 +218,10 @@ class PostRestControllerV1Test {
             throw new RuntimeException(e);
         }
     }
+
+
+
+
 
 
 
--- src/test/java/com/luxoft/osh/blog/service/PostServiceImplTest.java
+++ src/test/java/com/luxoft/osh/blog/service/PostServiceImplTest.java
@@ -2,29 +2,16 @@ package com.luxoft.osh.blog.service;
 
 import com.luxoft.osh.blog.entity.Post;
 import com.luxoft.osh.blog.repository.PostRepository;
-import com.luxoft.osh.blog.rest.PostRestControllerV1;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoSession;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
+import org.springframework.data.domain.PageRequest;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -36,19 +23,60 @@ class PostServiceImplTest {
     private PostRepository postRepository;
 
     @Test
-    void testGetAll() {
-        List<Post> posts = prepareForTestsAndReturnListOfPosts();
+    void testFindAll() {
+        PostServiceImpl postService = new PostServiceImpl(postRepository);
+
+        List<Post> posts = new ArrayList<>();
+
+        Post firstPost = Post.builder()
+                .id(1L)
+                .title("First post")
+                .content("A baba galamaga")
+                .star(true)
+                .build();
+        posts.add(firstPost);
+
+        Post secondPost = Post.builder()
+                .id(2L)
+                .title("Second post")
+                .content("Eins, zwei, Polizei")
+                .star(false)
+                .build();
+        posts.add(secondPost);
+
+        Post thirdPost = Post.builder()
+                .id(3L)
+                .title("Third post")
+                .content("Padav snih na porih, Kit zlipyv sobi pyrih.")
+                .star(true)
+                .build();
+        posts.add(thirdPost);
+
         Mockito.when(postRepository.findAll()).thenReturn(posts);
-        assertEquals(3, postRepository.findAll().size());
-        assertSame(posts, postRepository.findAll());
+
+        List<Post> actualPosts = postService.findAll();
+        assertNotNull(actualPosts);
+        assertEquals(3, actualPosts.size());
+        assertEquals("Eins, zwei, Polizei", actualPosts.get(1).getContent());
+        assertSame(thirdPost, actualPosts.get(2));
     }
 
     @Test
     void testGetById() {
-        Post post = prepareForTestsAndReturnListOfPosts().get(0);
-        Optional<Post> optionalPost = Optional.of(post);
-        Mockito.when(postRepository.findById(1L)).thenReturn(optionalPost);
-        assertEquals("First post", post.getTitle());
+        PostServiceImpl postService = new PostServiceImpl(postRepository);
+        Post post = Post.builder()
+                .id(1L)
+                .title("First post")
+                .content("A baba galamaga")
+                .star(true)
+                .build();
+
+        Mockito.when(postRepository.getById(1L)).thenReturn(post);
+        Post actualPost = postService.getById(1L);
+
+        assertEquals("First post", actualPost.getTitle());
+        assertEquals("A baba galamaga", actualPost.getContent());
+        assertTrue(actualPost.isStar());
     }
 
     @Test
@@ -58,52 +86,121 @@ class PostServiceImplTest {
 
     @Test
     void testSave() {
-        Post post = prepareForTestsAndReturnListOfPosts().get(0);
-        Mockito.when(postRepository.save(post)).thenReturn(post);
-        assertEquals("First post", post.getTitle());
+
     }
 
     @Test
     void testGetPostsWithStar() {
-    }
+        PostServiceImpl postService = new PostServiceImpl(postRepository);
 
-    @Test
-    void testFindByTitle() {
-    }
-
-    @Test
-    void testSortByTitle() {
-    }
-
-
-    private List<Post> prepareForTestsAndReturnListOfPosts() {
         List<Post> posts = new ArrayList<>();
 
         Post firstPost = Post.builder()
+                .id(1L)
                 .title("First post")
                 .content("A baba galamaga")
                 .star(true)
                 .build();
-        firstPost.setId(1L);
         posts.add(firstPost);
 
-        Post secondPost = Post.builder()
-                .title("Second post")
-                .content("Eins, zwei, Polizei")
-                .star(false)
-                .build();
-        secondPost.setId(2L);
-        posts.add(secondPost);
-
         Post thirdPost = Post.builder()
+                .id(3L)
                 .title("Third post")
                 .content("Padav snih na porih, Kit zlipyv sobi pyrih.")
                 .star(true)
                 .build();
-        thirdPost.setId(3L);
         posts.add(thirdPost);
 
-        return posts;
+        Mockito.when(postRepository.findAllByStar(true)).thenReturn(posts);
+
+        List<Post> actualPosts = postService.getPostsWithStar();
+        assertEquals(2, actualPosts.size());
+        assertEquals("First post", actualPosts.get(0).getTitle());
+        assertEquals("Third post", actualPosts.get(1).getTitle());
+        assertSame(thirdPost, actualPosts.get(1));
     }
+
+    @Test
+    void testFindByTitle() {
+        PostServiceImpl postService = new PostServiceImpl(postRepository);
+
+        List<Post> posts = new ArrayList<>();
+
+        Post firstPost = Post.builder()
+                .id(1L)
+                .title("First post")
+                .content("A baba galamaga")
+                .star(true)
+                .build();
+        posts.add(firstPost);
+
+        Post secondPost = Post.builder()
+                .id(2L)
+                .title("Second post")
+                .content("Eins, zwei, Polizei")
+                .star(false)
+                .build();
+        posts.add(secondPost);
+
+        Post thirdPost = Post.builder()
+                .id(3L)
+                .title("Third post")
+                .content("Padav snih na porih, Kit zlipyv sobi pyrih.")
+                .star(true)
+                .build();
+        posts.add(thirdPost);
+
+        Mockito.when(postRepository.findByTitle("Second post")).thenReturn(List.of(secondPost));
+
+        List<Post> actualPosts = postService.findByTitle("Second post");
+        assertEquals(1, actualPosts.size());
+        assertSame(secondPost, actualPosts.get(0));
+    }
+
+    @Test
+    void testSortByTitle() {
+        PostServiceImpl postService = new PostServiceImpl(postRepository);
+
+        List<Post> posts = new ArrayList<>();
+
+        Post firstPost = Post.builder()
+                .id(1L)
+                .title("First post")
+                .content("A baba galamaga")
+                .star(true)
+                .build();
+        posts.add(firstPost);
+
+        Post secondPost = Post.builder()
+                .id(2L)
+                .title("Second post")
+                .content("Eins, zwei, Polizei")
+                .star(false)
+                .build();
+        posts.add(secondPost);
+
+        Post thirdPost = Post.builder()
+                .id(3L)
+                .title("Third post")
+                .content("Padav snih na porih, Kit zlipyv sobi pyrih.")
+                .star(true)
+                .build();
+        posts.add(thirdPost);
+
+        Mockito.when(postRepository.findByOrderByTitleAsc(PageRequest.of(0, 4))).thenReturn(posts);
+
+        List<String> sortedTitles = new ArrayList<>(List.of(thirdPost.getTitle(), firstPost.getTitle(), secondPost.getTitle()));
+        Collections.sort(sortedTitles);
+
+        List<Post> actualPosts = postService.sortByTitle();
+
+        assertEquals(sortedTitles.get(0), actualPosts.get(0).getTitle());
+        assertEquals(sortedTitles.get(1), actualPosts.get(1).getTitle());
+        assertEquals(sortedTitles.get(2), actualPosts.get(2).getTitle());
+
+    }
+
+
+
 
 }
