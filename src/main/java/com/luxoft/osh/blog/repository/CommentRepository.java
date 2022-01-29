ad521e7edd5354926e47000082154617c0268add
Oleksandr Shevchenko <4336399@gmail.com>
2022-01-29 19:03:05 +0200

    rename methods in CommentRepository

--- src/main/java/com/luxoft/osh/blog/repository/CommentRepository.java
+++ src/main/java/com/luxoft/osh/blog/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.luxoft.osh.blog.repository;
 
 import com.luxoft.osh.blog.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,8 +11,11 @@ import java.util.List;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findAllByPost_Id(Long id);
+    @Query("select c from Comment c where c.id = :commentId and c.post.id = :postId")
+    Comment findByIdAndPostId(@Param("commentId") Long commentId, @Param("postId") Long postId);
 
-    Comment getByIdAndPost_Id(Long id, Long postId);
+
+    @Query("select c from Comment c where c.post.id = :postId")
+    List<Comment> findAllByPostId(@Param("postId") Long postId);
 
 }
